48183a82b51a24a2aaa4498b4b76216643d1d7a3
Artem Gusev <artem.gusev@rokolabs.com>
2021-07-24 19:23:23 +0300

    Added 'Recipes detail'

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -9,6 +9,7 @@ import { Recipe } from '../recipe.model';
 export class RecipesListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('Test Recipe', 'Some text in the description for the recipe.', 'https://stocksnap.io/photo/G8QICMKLUV/editor'),
+    new Recipe('Test Recipe 2', 'Some text in the description for the recipe 2.', 'https://img.delo-vcusa.ru/2011/03/DSC_0850.jpg'),
   ];
 
   constructor() { }
